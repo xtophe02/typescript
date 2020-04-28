3b73d37ffafeec7ad82444d7057e4db2e4abe537
Christophe Moreira <moreira.christophe@outlook.com>
2020-04-28 09:52:50 +0100

    export Sorter

---
+++ features/sort/src/Sorter.ts
@@ -0,0 +1,19 @@
+export class Sorter {
+  // collection: number[]
+  // constructor(collection:number[]){
+  //   this.collection = collection
+  // }
+  constructor(public collection: /*to do */){}
+  sort():void{
+    const {length} = this.collection
+    for (let i=0; i<length; i++){
+      for(let j=0; j<length-i-1;j++){
+        if(this.collection[j]>this.collection[j+1]){
+          const temp = this.collection[j]
+          this.collection[j] = this.collection[j+1]
+          this.collection[j+1] = temp 
+        }
+      }
+    }
+  }
+}
