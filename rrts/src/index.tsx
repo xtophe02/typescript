e4fc4a253f6404dda96a3294e45d3e7ed860cbf8
Christophe Moreira <christophe.moreira@outlook.com>
2020-05-20 13:20:28 +0100

    added functional comp with TS

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -4,10 +4,38 @@ import ReactDOM from "react-dom";
 interface AppProps {
   color?: string;
 }
+interface AppState {
+  counter: number;
+}
 
-class App extends React.Component<AppProps> {
+// const App = ({ color }: AppProps): JSX.Element => {
+//   return <div>{color}</div>;
+// };
+
+class App extends React.Component<AppProps, AppState> {
+  // state = { counter: 0 };
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+  handleClick = () => {};
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        {this.props.color}
+        <button
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          ++
+        </button>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter - 1 })}
+        >
+          --
+        </button>
+        result: {this.state.counter}
+      </div>
+    );
   }
 }
 
